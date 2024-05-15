c0f3f1186f0c02eba1026ae1caa00fbd8ef33bf3
ledsouza <leandro.souza.159@gmail.com>
2024-05-15 14:21:26 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    try {
+        elemento.play();
+    } catch (error) {
+        if (error instanceof TypeError) {
+            console.log("Seletor inválido! Verifique se ele existe e se é está associado a tag <audio>");
+        }
+    }
+}
+
+const listaDeTecla = document.querySelectorAll(".tecla")
+
+for (let index = 0; index < listaDeTecla.length; index++) {
+    const tecla = listaDeTecla[index];
+    const tipoDeTecla = tecla.classList[1];
+    const idAudio = `#som_${tipoDeTecla}`;
+    
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }    
+}
